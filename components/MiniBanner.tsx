f99c0f2fd788a0438897d4363b479af2d7d485cb
aabhasxgurung <aabhasgrg46@gmail.com>
2025-12-18 13:01:12 +0545

    refactor: update MiniBanner list item key to use array index

--- components/MiniBanner.tsx
+++ components/MiniBanner.tsx
@@ -66,7 +66,7 @@ const MiniBanner = () => {
         <div className="grid md:grid-cols-2 lg:grid-cols-5 gap-8">
           {brands.map((brand, index) => (
             <motion.div
-              key={brand.id}
+              key={index}
               initial={{ opacity: 0, y: 20 }}
               whileInView={{ opacity: 1, y: 0 }}
               viewport={{ once: true }}
